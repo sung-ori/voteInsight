4e8623df9e1b8b539ae49445713fa53d3dae088b
sung_ori <l9707so@gmail.com>
2024-01-28 16:00:24 +0900

    프로젝트 생성 완료

---
+++ src/main/java/com/kisscotp/voteInsight/VoteInsightApplication.java
@@ -0,0 +1,13 @@
+package com.kisscotp.voteInsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteInsightApplication.class, args);
+	}
+
+}
