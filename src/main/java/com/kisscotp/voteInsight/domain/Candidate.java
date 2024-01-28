a9d8a2a17c2a2901be6b15e91cc43c9259617cc1
sung_ori <l9707so@gmail.com>
2024-01-28 22:37:36 +0900

    선거 객체, 후보 객체 작성

---
+++ src/main/java/com/kisscotp/voteInsight/domain/Candidate.java
@@ -0,0 +1,37 @@
+package com.kisscotp.voteInsight.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "candidate")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long    candidateidx;   // 후보 인덱스
+
+    @Column
+    private Long    useridx;        // 후보의 유저 인덱스
+    
+    @Column
+    private Long    electionidx;    // 참가하는 선거의 인덱스
+    
+    @Column
+    private Long    candinumber;    // 기호 몇 번
+
+    @Column
+    private String  imgpath;        // 홍보 이미지 경로
+}
---
+++ src/main/java/com/kisscotp/voteInsight/domain/Election.java
@@ -0,0 +1,57 @@
+package com.kisscotp.voteInsight.domain;
+
+import java.time.LocalDateTime;
+
+import com.kisscotp.voteInsight.domain.enums.GroupType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "election")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Election {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long            electionidx;    // 인덱스
+
+    @Column
+    private String          title;          // 선거 제목
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private GroupType       grouptype;      // 투표 대상 그룹
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private String          posterpath;     // 포스터 경로
+
+    @Column
+    private char            progress;       // 진행 상황 0 = 준비, 1 = 진행중, 2 = 투표 종료 열람 가능, 3 = 열람기간 만료
+
+    @Column
+    private LocalDateTime   createdate;     // 선거 생성일
+
+    @Column
+    private LocalDateTime   startdate;      // 투표 시작일
+
+    @Column
+    private LocalDateTime   daeline;        // 투표 종료일
+
+    @Column
+    private LocalDateTime   enddate;        // 열람 마감일
+}
