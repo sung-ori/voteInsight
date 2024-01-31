8f078a8def1d75f264875c53337decb8ed64138b
sung_ori <l9707so@gmail.com>
2024-01-31 11:28:23 +0900

    권한 명 변경, board 객체수정

--- src/main/java/com/kisscotp/voteInsight/domain/Board.java
+++ src/main/java/com/kisscotp/voteInsight/domain/Board.java
@@ -3,10 +3,22 @@ package com.kisscotp.voteInsight.domain;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
     
     @Id
