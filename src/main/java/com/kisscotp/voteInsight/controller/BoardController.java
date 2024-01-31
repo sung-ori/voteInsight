a44f476b37475baed11f63247f766f2783309295
tjdwlsgml1025 <gigaginny1025@gmail.com>
2024-02-01 01:42:34 +0900

    ui수정

---
+++ src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.kisscotp.voteInsight.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class BoardController {
+   
+
+}
