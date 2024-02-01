313483a22149881ae6460528c29aaebc48b18eb8
tjdwlsgml1025 <gigaginny1025@gmail.com>
2024-02-01 22:10:06 +0900

    상세보기수정중

--- src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
+++ src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
@@ -1,29 +1,37 @@
 package com.kisscotp.voteInsight.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kisscotp.voteInsight.domain.Board;
+import com.kisscotp.voteInsight.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class BoardController {
+
+    @Autowired
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
    
     //글 작성 뷰
     @GetMapping("/board/write") 
     public String boardWriteForm(){
-        return "/boardWrite";
+        return "boardWrite";
     }
 
     //글작성
     @PostMapping("/board/writepro")
-    public String boardwritePro(Board board){ //String title, String content
-        System.out.println("제목: " +board.getTitle());
-        System.out.println("내용: " +board.getContents());
-        return "/boardList";
+    public String boardwritePro(Board board){ 
+
+      boardService.write(board);
+
+        return "redirect:boardList";
     }
 
 
