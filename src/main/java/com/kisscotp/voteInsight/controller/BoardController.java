4a2b5228eeba6cc003337a1ebd92a24b95032a45
sung_ori <l9707so@gmail.com>
2024-02-04 19:42:22 +0900

    타임리프 템플릿 분리

--- src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
+++ src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.kisscotp.voteInsight.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kisscotp.voteInsight.domain.Board;
+import com.kisscotp.voteInsight.domain.Users;
 import com.kisscotp.voteInsight.service.BoardService;
+import com.kisscotp.voteInsight.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +21,17 @@ public class BoardController {
 
     @Autowired
     BoardService boardService;
-     
+    @Autowired
+    UserService userService;
     
     //글 작성 뷰
     @GetMapping("/board/write") 
-    public String boardWriteForm(){
-        return "boardWrite";
+    public String boardWriteForm(@AuthenticationPrincipal UserDetails user, Model model){
+        if(user != null) {
+            Users loginUser = userService.getUser(user.getUsername());
+            model.addAttribute("user", loginUser);
+        }
+        return "/board/boardWrite";
     }
 
     //글작성
--- src/main/java/com/kisscotp/voteInsight/controller/IndexController.java
+++ src/main/java/com/kisscotp/voteInsight/controller/IndexController.java
@@ -28,8 +28,11 @@ public class IndexController {
     
 
     @GetMapping("/")
-    public String main() {
-        
+    public String main(@AuthenticationPrincipal UserDetails user, Model m) {
+                if(user != null) {
+            Users loginUser = userService.getUser(user.getUsername());
+            m.addAttribute("user", loginUser);
+        }
 
         return "/indexUser";
     }
@@ -41,33 +44,33 @@ public class IndexController {
 
     //공지사항 목록
     @GetMapping("/board/list")
-    public String boardlist(Model model) {
+    public String boardlist(@AuthenticationPrincipal UserDetails user,Model model) {
+        // 로그인 했으면 유저를 구분해줘야하고
+        // 네브바에 표시 내용이 달라지므로 
+        // 로그인 후 보는 페이지들에는  
+        // @AuthenticationPrincipal UserDetails user 로
+        // 현재 로그인중인 유저의 정보를 모델에 담아 보내줘야 함다 !
+        // 사실 이건 사용자 이름 표시 안해줄 거면 할 필요 없긴 합니다,,,
+        if(user != null) {
+            Users loginUser = userService.getUser(user.getUsername());
+            model.addAttribute("user", loginUser);
+        }
         model.addAttribute("boards", boardService.boardlist());
-        return "/boardList";
+        return "/board/boardList";
     }
 
     //공지사항 상세조회
     @GetMapping("/board/view") 
-    public String boardView(@RequestParam(name="boardidx", defaultValue="0") Long idx, Model model) {
-                Board board = boardService.boardview(idx);
-               model.addAttribute("board", board);
-        return "/boardView";
-}
-//navbar
-    @GetMapping("/navBar")
-    public String navBar(@AuthenticationPrincipal UserDetails user, Model m) {
-        // 일단 여기로 지정해 놓았음!
+    public String boardView(@AuthenticationPrincipal UserDetails user, 
+                            @RequestParam(name="boardidx", defaultValue="0") Long idx,
+                             Model model) {
         if(user != null) {
             Users loginUser = userService.getUser(user.getUsername());
-            m.addAttribute("user", loginUser);
+            model.addAttribute("user", loginUser);
         }
-        
-        return "/fragments/navBar";
-    }
-    
-    @GetMapping("/footer")
-    public String footer() {
-        
-        return"/fragments/footer";
-    }
+        Board board = boardService.boardview(idx);
+        model.addAttribute("board", board);
+        return "/board/boardView";
+}
+
 }
