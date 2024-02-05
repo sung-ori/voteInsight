0344531cd00a400f63bfbd99c05c925fda1737b1
tjdwlsgml1025 <gigaginny1025@gmail.com>
2024-02-05 15:46:27 +0900

    상세+삭제완료

--- src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
+++ src/main/java/com/kisscotp/voteInsight/controller/BoardController.java
@@ -1,74 +1,79 @@
 package com.kisscotp.voteInsight.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kisscotp.voteInsight.domain.Board;
 import com.kisscotp.voteInsight.domain.Users;
 import com.kisscotp.voteInsight.service.BoardService;
 import com.kisscotp.voteInsight.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@RequestMapping("/board")
 @Slf4j
 public class BoardController {
 
     @Autowired
-    BoardService boardService;
+    private BoardService boardService;
 
     @Autowired
-    UserService userService;
-    
+    private UserService userService;
 
-    //글삭제
-    @GetMapping("/board/delete/{boardidx}")
-    public String boardDelete(@AuthenticationPrincipal UserDetails user, 
-                                @RequestParam(name="boardidx", defaultValue="0") Long idx, 
-                                Model model) {
+    // 글 삭제
+    @PostMapping("/delete/{boardidx}")
+    public String boardDelete(@AuthenticationPrincipal UserDetails user,
+                              @PathVariable Long boardidx,
+                              Model model) {
+        if (user != null) {
+            Users loginUser = userService.getUser(user.getUsername());
+            model.addAttribute("user", loginUser);
+        }
 
+        boardService.boardDelete(boardidx);
+
+        return "redirect:/board/list";
+    }
+
+    //글 작성 뷰
+    @GetMapping("/board/write") 
+    public String boardWriteForm(@AuthenticationPrincipal UserDetails user, Model model){
         if(user != null) {
             Users loginUser = userService.getUser(user.getUsername());
             model.addAttribute("user", loginUser);
-        }  
-        boardService.boardDelete(idx);
-
-        return "redirect:/board/boardlist";
+        }
+        return "/board/boardWrite";
     }
 
-    //    //글작성
-    //    @PostMapping("/board/writepro")
-    //    public String boardwritePro(Board board){ 
+    //글작성
+    @PostMapping("/board/writepro")
+    public String boardwritePro(Board board){ 
+
+      boardService.write(board);
+
+        return "redirect:boardList";
+    }
+
    
-    //      boardService.write(board);
-   
-    //        return "redirect:boardList";
-    //    }
 
-    //        //글 작성 뷰
-    // @GetMapping("/board/write") 
-    // public String boardWriteForm(@AuthenticationPrincipal UserDetails user, Model model){
-    //     if(user != null) {
-    //         Users loginUser = userService.getUser(user.getUsername());
-    //         model.addAttribute("user", loginUser);
-    //     }
-    //     return "/board/boardWrite";
-    // }
-
-
-    //글 수정 뷰 
-    // @GetMapping("/board/modify/{idx}")
-    // public String boardmodify(@PathVariable("idx") Long idx,Model model){
+    // 글 수정 뷰 
+    // @GetMapping("/modify/{boardidx}")
+    // public String boardModify(@PathVariable("idx") Long idx, Model model){
     //     model.addAttribute("board", boardService.boardview(idx));
     //     return "boardmodify";
     // }
 
-    // // 게시글 수정
-    // @PutMapping("/board/update/{boardidx}")
+    // 게시글 수정
+    // @PutMapping("/update/{boardidx}")
     // public Long update(@PathVariable Long idx, @RequestBody Board board) {
     //     return boardService.update(idx, board);
     // }
-
-
-
 }
