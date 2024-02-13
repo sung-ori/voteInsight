85b584fb92bd294540b80067a158617b1d862d25
tjdwlsgml1025 <gigaginny1025@gmail.com>
2024-02-13 15:40:00 +0900

    공지사항리스트페이징

--- src/main/java/com/kisscotp/voteInsight/controller/IndexController.java
+++ src/main/java/com/kisscotp/voteInsight/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.kisscotp.voteInsight.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -55,22 +56,27 @@ public class IndexController {
     }
 
     //공지사항 목록
-    @GetMapping("/board/list")
-    public String boardlist(@AuthenticationPrincipal UserDetails user,Model model) {
-        // 로그인 했으면 유저를 구분해줘야하고
-        // 네브바에 표시 내용이 달라지므로 
-        // 로그인 후 보는 페이지들에는  
-        // @AuthenticationPrincipal UserDetails user 로
-        // 현재 로그인중인 유저의 정보를 모델에 담아 보내줘야 함다 !
-        // 사실 이건 사용자 이름 표시 안해줄 거면 할 필요 없긴 합니다,,,
-        if(user != null) {
-            Users loginUser = userService.getUser(user.getUsername());
-            model.addAttribute("user", loginUser);
-        }
-        model.addAttribute("boards", boardService.boardlist());
-        
-        return "/board/boardList";
+  @GetMapping("/board/list")
+public String boardlist(@AuthenticationPrincipal UserDetails user,
+                        @RequestParam(name = "page", defaultValue = "0", required = true) int page,
+                        Model model) {
+    // 로그인 했으면 유저를 구분해줘야하고
+    // 네브바에 표시 내용이 달라지므로 
+    // 로그인 후 보는 페이지들에는  
+    // @AuthenticationPrincipal UserDetails user 로
+    // 현재 로그인중인 유저의 정보를 모델에 담아 보내줘야 함다 !
+    // 사실 이건 사용자 이름 표시 안해줄 거면 할 필요 없긴 합니다,,,
+
+    if(user != null) {
+        Users loginUser = userService.getUser(user.getUsername());
+        model.addAttribute("user", loginUser);
     }
+
+    Page<Board> board = boardService.boardList(page);
+    model.addAttribute("boards", board);
+    
+    return "/board/boardList";
+}
 
     //공지사항 상세조회
     @GetMapping("/board/view") 
